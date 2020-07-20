8ace8bb15ff79cf3b455c1b276abbd66778dac94
aman rathore <aman@kali.aman>
2020-07-20 13:02:30 -0400

    Count_Number

--- Count_Character.py
+++ Count_Character.py
@@ -1,5 +1,12 @@
 str1 = 'Hello World'
-str2 = 'Aman Rathore'
+#str2 = 'Aman Rathore'
 
 
+#count a letter in string 
 print(str1.count('o'));
+
+
+#count a letter a number
+l = [1,2,3,4,5,6,7,7,7,7];
+print(l.count(7));
+
