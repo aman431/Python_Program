697beb5234f806d296913ce6cb27fc5673f80294
aman rathore <aman@kali.aman>
2020-07-20 13:26:15 -0400

    Update

--- Basic_prog.py
+++ Basic_prog.py
@@ -5,10 +5,25 @@ def count_str():
     ans1 = l1.count(7);
     return "String ",ans," Integer ",ans1
 
-print(count_str())
+#print(count_str())
 
 #counting each word
 str1="Hello World"
 for i in str1:
     a1 = str1.count(i)
     #print(a1)
+
+
+#Sum of digit
+def sum_of_num(n):
+    sum=0
+    while n!=0:
+        rem=n%10
+        sum = sum+rem
+        n=n/10
+    return int(sum)
+
+n=int(input("Enter the number"))
+print(sum_of_num(n))
+
+    
