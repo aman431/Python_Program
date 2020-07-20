a8d87066b4f9da8fc0d55cd64cfc325e6442139f
aman rathore <aman@kali.aman>
2020-07-20 13:54:45 -0400

    Update

--- Basic_prog.py
+++ Basic_prog.py
@@ -1,3 +1,4 @@
+#count single character using count method
 def count_str():
     str1 = 'Hello World'
     l1 = [12,2,3,4,5,6,6,7,7,7]
@@ -7,14 +8,24 @@ def count_str():
 
 #print(count_str())
 
-#counting each word
+
+#counting each word by using loop
 str1="Hello World"
 for i in str1:
     a1 = str1.count(i)
     #print(a1)
 
+#count all character and store in Object with a Name of a Character
+str1 = "Hello World"
+Obj = {}
+for i in str1:
+    if i in Obj:
+        Obj[i] += 1
+    else:
+        Obj[i] = 1
+print(Obj)
 
-#Sum of digit
+#Sum of digit 
 def sum_of_num(n):
     sum=0
     while n!=0:
@@ -25,6 +36,8 @@ def sum_of_num(n):
 
 #n=int(input("Enter the number"))
 #print(sum_of_num(n))
+
+
 
 #class program to calculate a factor
 class factor:
