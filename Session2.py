47741ff4c7e81a18b60e6999687f6044d6e6544d
aman rathore <aman@kali.aman>
2020-07-21 02:32:33 -0400

    Update

--- Session2.py
+++ Session2.py
@@ -1,3 +1,5 @@
+#check a number is Even or Odd
+
 class Even_Odd:
     def accept(self):
         self.n=int(input("Enter the Number"))
@@ -14,9 +16,31 @@ class Even_Odd:
 
     def display(self):
         print("The list of ", self.l1)
-
-
+'''
 c = Even_Odd()
+c.accept()
+c.check()
+c.display()
+'''
+
+#Store the String in array which is in the position of even Number
+class Count_String:
+    def accept(self):
+        self.str1 = input("Enter the String")
+
+    def check(self):
+
+        self.str2 = ''
+        for i in range(len(self.str1)):
+            if(i%2==0):
+                self.str2 = self.str2+ self.str1[i]
+
+    def display(self):
+        print(self.str1)
+        print(self.str2)
+
+
+c = Count_String()
 c.accept()
 c.check()
 c.display()
